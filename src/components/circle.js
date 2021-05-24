d8ace9d59c4b933ff4208070a8da68bf9d6fc682
Mayankshukla1398 <83172828+Mayankshukla1398@users.noreply.github.com>
2021-05-24 18:22:15 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
 // import using require
 
 // declare class
+import { Shape } from "./shape";
+
+// declare class
+class Circle extends Shape {
+  calculateArea() {
+    console.log("cal in circle");
+  }
+}
+
+// export class using module.exports
+module.exports = Circle;
 
 
 // export class using module.exports
