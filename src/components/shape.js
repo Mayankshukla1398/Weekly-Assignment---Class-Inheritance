a35a55f0b8b655c607d6125e4dd3d6d5fc844650
Mayankshukla1398 <83172828+Mayankshukla1398@users.noreply.github.com>
2021-05-24 18:21:38 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
 // declare class
+  
+import React from "react";
+class Shape extends React.Component {
+  constructor(props) {
+    super(props);
+    this.color = "blue";
+  }
+  drawShape() {
+    console.log("drawing shape");
+  }
+  calculateArea() {
+    console.log("cal in shape");
+  }
+}
+// export class using module.exports
+module.exports = Shape;
 
 // export class using module.exports
