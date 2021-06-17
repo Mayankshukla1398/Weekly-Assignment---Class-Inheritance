4c9dbd75b66b3dbde7a2805a11de3399159310a2
Mayankshukla1398 <83172828+Mayankshukla1398@users.noreply.github.com>
2021-06-17 23:32:00 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,15 +1,10 @@
-// declare class
-class Shape {
-    constructor(color) {
-        this.color=color;
-    }
-    drawShape() {
-        return color;
-    }
-    calculateArea() {
+// export class
+export default class Shape {
+  color = null;
 
-    }
+  constructor(color) {
+    this.color = color;
+  }
+  drawShape = function () {};
+  calculateArea = function () {};
 }
-
-module.exports = Shape;
-// export class using module.exports
