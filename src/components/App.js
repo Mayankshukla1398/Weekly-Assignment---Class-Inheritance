295fbb04e7f4a62f4cb73f340c0595bb4f8520d0
Mayankshukla1398 <83172828+Mayankshukla1398@users.noreply.github.com>
2021-06-18 13:42:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 const React = require("react");
 const Styles =require('../styles/App.css');
+const Shape =require("./shape");
+const Circle =require("./circle");
 
 class App extends Component {
     render() {
